706bf9b29466bc09d0b1879dc7bc9aa2d625a69e
Chinwenma <okoriechinwe1327@gmail.com>
2023-05-04 05:21:22 -0800

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,24 @@
 import "./login.scss";
 
-export default function Login (){
+export default function Login() {
     return (
         <div className="login">
-            LOGIN
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam fugit consectetur </p>
+                    <span>Don`t have an account?</span>
+                    <button>Sign Up</button>
+                </div>
+                <div className="right">
+                    <h1>login</h1>
+                    <form >
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
