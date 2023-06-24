f772c1f3fa7bc0afe90808c44b2086eb14169964
Chinwenma <okoriechinwe1327@gmail.com>
2023-06-23 21:25:49 -0800

    Ht

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -27,3 +27,5 @@ export default function Login() {
         </div>
     )
 }
+
+
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,29 +1,67 @@
+// import { Link } from "react-router-dom";
+// import "./register.scss";
+// export default function Regsiter(){
+//     return (
+//          <div className="register">
+//             <div className="card">
+//             <div className="left">
+//                     <h1>Matrix Social</h1>
+//                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam fugit consectetur </p>
+//                     <span>Have an account already?</span>
+//                     <Link to="/login">
+//                     <button>Login</button>
+//                     </Link>  
+//                 </div>
+//                 <div className="right">
+//                     <h1>Register</h1>
+//                     <form >
+//                         <input type="text" placeholder="Username" />
+//                         <input type="email" placeholder="Email" />
+//                         <input type="password" placeholder="Password" />
+//                         <input type="text" placeholder="Name" />
+//                         <button>Sign Up</button>
+//                     </form>
+//                 </div>
+                
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
 import { Link } from "react-router-dom";
 import "./register.scss";
-export default function Regsiter(){
-    return (
-         <div className="register">
-            <div className="card">
-            <div className="left">
-                    <h1>Matrix Social</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam fugit consectetur </p>
-                    <span>Have an account already?</span>
-                    <Link to="/login">
-                    <button>Login</button>
-                    </Link>  
-                </div>
-                <div className="right">
-                    <h1>Register</h1>
-                    <form >
-                        <input type="text" placeholder="Username" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <input type="text" placeholder="Name" />
-                        <button>Sign Up</button>
-                    </form>
-                </div>
-                
-            </div>
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Matrix</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+          <button>Login</button>
+          </Link>
         </div>
-    )
-}
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
