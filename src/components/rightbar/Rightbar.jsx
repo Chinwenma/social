6e4da8b5bdc4f170bfeee1f1337db8ba3e18df1d
Chinwenma <okoriechinwe1327@gmail.com>
2023-06-27 15:43:38 -0800

    right

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -7,8 +7,19 @@ export default function Rightbar() {
                     <span>People You May Konw</span>
                     <div className='user'>
                         <div className='userInfo'>
-                        <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt='user'  />
-                        
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt='user' />
+
+                            <span>RuhamaKamso May</span>
+                        </div>
+                        <div className='buttons'>
+                            <button>follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt='user' />
+
                             <span>RuhamaKamso May</span>
                         </div>
                         <div className='buttons'>
@@ -17,9 +28,19 @@ export default function Rightbar() {
                         </div>
                     </div>
                 </div>
+                <div className='item'>
+                    <span>Latest Activities</span>
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt='user' />
+
+                            <p><span>Ruhama Kamso </span></p>
+                        </div>
+                       <span>1 min ago</span>
+                    </div>
+                </div>
+
             </div>
-
-
         </div>
     )
 }
